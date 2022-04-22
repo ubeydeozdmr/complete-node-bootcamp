cf14f1b553ae953e4d4bfec63d95c7ed3407b52c
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-04-22 15:37:03 +0300

    Building a (Very) Simple API

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -30,6 +30,11 @@ const url = require('url');
 
 ///////////////////////////////
 // ! SERVER
+//
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -37,6 +42,9 @@ const server = http.createServer((req, res) => {
     res.end('This is the OVERVIEW');
   } else if (pathName === '/product') {
     res.end('This is the PRODUCT');
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
