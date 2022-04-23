06c7cd4ba24c68bcae44b2ddb8f7b1c40e7ba372
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-04-23 15:27:20 +0300

    Parsing Variables from URLs

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -64,10 +64,10 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   // ? Overview page
-  if (pathName === '/' || pathName === '/overview') {
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' });
 
     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
@@ -76,11 +76,15 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     // ? Product page
-  } else if (pathName === '/product') {
-    res.end('This is the PRODUCT');
+  } else if (pathname === '/product') {
+    const product = dataObj[query.id];
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output);
 
     // ? API
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
 
