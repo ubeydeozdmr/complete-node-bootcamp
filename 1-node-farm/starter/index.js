4d9eb357c29a6301853da81c3b4a64b947b1aba5
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-04-24 13:06:05 +0300

    Setting up Prettier in VS Code

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -37,12 +37,10 @@ const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   'utf-8'
 );
-
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
   'utf-8'
 );
-
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
   'utf-8'
@@ -60,7 +58,9 @@ const server = http.createServer((req, res) => {
 
   // ? Overview page
   if (pathname === '/' || pathname === '/overview') {
-    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
 
     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
@@ -70,14 +70,18 @@ const server = http.createServer((req, res) => {
     // ? Product page
   } else if (pathname === '/product') {
     const product = dataObj[query.id];
-    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
     const output = replaceTemplate(tempProduct, product);
 
     res.end(output);
 
     // ? API
   } else if (pathname === '/api') {
-    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
     res.end(data);
 
     // ? Not found
