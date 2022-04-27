7cf68ccc579cbdbc45862b58cdf067fc259dfd2b
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-04-27 03:28:15 +0300

    Consuming Promises with Async/Await

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -19,6 +19,25 @@ const writeFilePro = (file, data) => {
   });
 };
 
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body.message);
+
+    await writeFilePro('dog-img.txt', res.body.message);
+    console.log('Random dog image saved to file');
+  } catch (err) {
+    console.log(err);
+  }
+};
+getDogPic();
+
+/*
 readFilePro(`${__dirname}/dog.txt`)
   .then(data => {
     console.log(`Breed: ${data}`);
@@ -34,3 +53,4 @@ readFilePro(`${__dirname}/dog.txt`)
   .catch(err => {
     console.log(err);
   });
+*/
