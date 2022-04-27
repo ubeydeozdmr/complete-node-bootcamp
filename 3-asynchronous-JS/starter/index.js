9f38987e597de3f597c541f7d7ea8b6efb5e06d9
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-04-27 03:46:57 +0300

    Returning Values from Async Functions

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -33,9 +33,34 @@ const getDogPic = async () => {
     console.log('Random dog image saved to file');
   } catch (err) {
     console.log(err);
+
+    throw err;
   }
+  return '2: READY 🐶';
 };
-getDogPic();
+
+(async () => {
+  try {
+    console.log('1: Will get dog pics!');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done getting dog pics!');
+  } catch (err) {
+    console.log('ERROR 💥');
+  }
+})();
+
+/*
+console.log('1: Will get dog pics!');
+getDogPic()
+  .then(x => {
+    console.log(x);
+    console.log('3: Done getting dog pics!');
+  })
+  .catch(err => {
+    console.log('ERROR 💥');
+  });
+*/
 
 /*
 readFilePro(`${__dirname}/dog.txt`)
