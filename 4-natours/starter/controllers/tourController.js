f310ee521581558ee2c6f3e890803fb94a8f4f00
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-05-13 22:50:54 +0300

    Setting up ESLint + Prettier in VS Code

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -57,12 +57,7 @@ exports.createTour = (req, res) => {
   // console.log(req.body);
 
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign(
-    {
-      id: newId,
-    },
-    req.body
-  );
+  const newTour = Object.assign({ id: newId }, req.body);
 
   tours.push(newTour);
 
