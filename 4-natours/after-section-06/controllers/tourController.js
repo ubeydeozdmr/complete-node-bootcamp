c98d44e1cfc24c31f444a00b2004ec11ac9f129b
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-05-17 14:09:08 +0300

    Making the API Better: Pagination

--- 4-natours/after-section-06/controllers/tourController.js
+++ 4-natours/after-section-06/controllers/tourController.js
@@ -33,6 +33,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // const tours = await Tour.find(req.query);
 
     // const tours = await Tour.find({
@@ -48,6 +60,7 @@ exports.getAllTours = async (req, res) => {
 
     // EXECUTE QUERY
     const tours = await query;
+    // query.sort().select().skip().limit()
 
     // SEND RESPONSE
     res.status(200).json({
