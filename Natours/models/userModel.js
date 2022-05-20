f1e819c7d04d6c481c98fd65c49e2c2045fa8c6a
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-05-21 02:44:19 +0300

    Logging in Users

--- Natours/models/userModel.js
+++ Natours/models/userModel.js
@@ -20,7 +20,8 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please provide a password'],
-    minLength: 8
+    minLength: 8,
+    select: false
   },
   passwordConfirm: {
     type: String,
@@ -46,6 +47,13 @@ userSchema.pre('save', async function(next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
