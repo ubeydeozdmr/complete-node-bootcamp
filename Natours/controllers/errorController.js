7a0c34a7503149c6f29e76dabc04c996c9d221ec
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-05-20 13:46:41 +0300

    Fix for the latest version of MongoDB

--- Natours/controllers/errorController.js
+++ Natours/controllers/errorController.js
@@ -44,7 +44,7 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err };
+    let error = Object.assign(err);
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     sendErrorProd(error, res);
